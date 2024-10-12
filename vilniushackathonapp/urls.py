71c18297daa59ee9632625d95171f26737a0ef09
AUGEACC35 <augeacc35@gmail.com>
2024-10-12 23:01:54 +0300

    Create Apie page

--- vilniushackathonapp/urls.py
+++ vilniushackathonapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('map', views.map, name='map'),
     path('greenwaste', views.greenwaste, name='greenwaste'),
     path('find-container/', views.find_container, name='find_container'),
+    path('apie', views.apie, name='apie'),
 ]
